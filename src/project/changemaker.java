21463cea0f8d5ec51a606d8f198734525e5769a0
Jacob Paul <jacobpaul787@gmail.com>
2017-06-04 17:06:52 -0600

    fixed error in main

--- src/project/changemaker.java
+++ src/project/changemaker.java
@@ -3,23 +3,30 @@ package project;
 import java.util.Scanner;
 
 public class changemaker {
-
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
-		Double cost, amtGivin, changetotal;
+		Double cost, amtgivin, change, changetotal;
 		
 		System.out.println("Please enter a purchase total" + "\n$");
 		cost = keyboard.nextDouble();		
 		System.out.println("Please enter the amount of money given." + "\n$");
-		amtGivin = keyboard.nextDouble();		
+		amtgivin = keyboard.nextDouble();		
 		
-		if (cost > amtGivin){
-			System.out.println("You still owe" + " $" + -(amtGivin - cost));
+		if (cost > amtgivin){
+			System.out.println("You still owe" + " $" + -(amtgivin - cost));
 		}
-		if (cost == amtGivin){
+		if (cost == amtgivin){
 			System.out.println("You gave exact change. Thanks for making my life easy!");
+		}
+		if (cost < amtgivin){
+			change = (amtgivin - cost);
+			calculator(change);
 		}
 		
 	}
-
+	public static void calculator(double change) {
+		Double penny, nickle, dime, quarter, fifty;
+		
+//		if ((change * 100 ) % )
+	}
 }
